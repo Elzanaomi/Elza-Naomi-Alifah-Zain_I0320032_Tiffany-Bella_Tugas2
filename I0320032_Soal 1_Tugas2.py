9719f848be0775704c6aec5de6deb2b83bae4474
Elzanaomi <80144770+Elzanaomi@users.noreply.github.com>
2021-03-12 20:45:42 +0700

    Add files via upload

---
+++ I0320032_Soal 1_Tugas2.py
@@ -0,0 +1,62 @@
+#menampilkan informasi program
+print ("Luas Persegi Panjang")
+
+#Input nilai panjang dan lebar
+P = float(input("Masukkan nilai panjang: "))
+L = float(input("Masukkan nilai lebar: "))
+
+#Menghitung luas persegi panjang
+Luas_persegi_panjang = P * L
+
+#Menampilkan hasil perhitungan luas persegi panjang
+print ("Luas persegi panjang adalah",Luas_persegi_panjang)
+
+#Menampilkan informasi Luas Lingkaran
+print ("Menghitung Luas Lingkaran")
+
+#Input nilai jari-jari
+r = float(input("Masukkan nilai jari-jari: "))
+
+#Menghitung luas lingkaran
+Luas_lingkaran = 3.14 * (r ** 2)
+
+#Menampilkan hasil perhitungan luas lingkaran
+print ("Luas lingkaran adalah",Luas_lingkaran)
+
+#Menampilkan informasi luas kubus
+print ("Menghitung Luas Kubus")
+
+#Input nilai sisi
+s = float(input("Masukan nilai sisi: "))
+
+#Menghitung luas kubus
+Luas_kubus = 6 * (s ** 2)
+
+#Menampilkan hasil perhitungan luas kubus
+print ("Luas kubus adalah",Luas_kubus)
+
+#Menampilkan informasi pengkonversian suhu celcius ke farenheit
+print ("Konversi Suhu Celcius ke Farenheit")
+
+#Input nilai suhu dalam celcius
+C = int(input("Masukan nilai suhu dalam celcius: "))
+
+#Menghitung hasil pengkonversian
+C_F = 9 * (C + 32) / 5
+
+#Menampilkan hasil hitung pengkonversian
+print ("Besar suhu dalam Farenheit adalah",C_F)
+
+#Menampilkan informasi pengkonversian suhu Reamur ke Kelvin
+print ("Konversi Suhu Reamur ke Kelvin")
+
+#Input nilai suhu dalam Reamur
+R = int(input("Masukan nilai suhu dalam Reamur: "))
+
+#Menghitung hasil pengkonversian
+R_K = 5 * (R + 273) / 4
+
+#Menampilkan hasil hitung pengkonversian
+print ("Besar suhu dalam Kelvin adalah",R_K)
+
+
---
+++ I0320032_Soal 2_Tugas2.py
@@ -0,0 +1,33 @@
+print ("Haloo, namaku Elza Naomi Alifah Zain.")
+print ("Biasanya teman-teman memanggilku Echa.")
+print ("Aku merupakan anak pertama dari dua bersaudara.")
+print ("Aku memiliki satu adik perempuan.")
+print ("Saat ini aku belum menetap di Solo.")
+print ("Aku masih menetap bersama orang tuaku di Bogor.")
+#Memasukkan data alamat
+cluster = "Florence"
+blok_rumah = "H6"
+no_rumah = 34
+print ("Aku bertempat tinggal di Kota Wisata,",cluster,",blok", blok_rumah,", No.",no_rumah,".")
+print ("Kini aku berkuliah di Universitas Sebelas Maret.")
+print ("Disana aku mengambil jurusan Teknik Industri.")
+print ("Aku mengambil jurusan Teknik Industri karena disini kami mempelajari berbagai hal dan itu sangat menarik.")
+#Memasukkan tanggal dan tahun lahir
+Tahun_lahir = int((1300 * 2)- 597.001)
+Tanggal_lahir = 12
+#Memasukkan umur saya saat ini, yaitu 18 tahun 6 bulan
+umur_tahun = int(36.5 / 2)
+umur_bulan = "September"
+Umur_aku = float(18.6 * 12)
+print ("Aku lahir di Kebumen pada", Tanggal_lahir,umur_bulan,Tahun_lahir,".")
+print ("Saat ini, aku berusia", Umur_aku,"bulan.")
+#Memasukkan data ukuran sepatu
+Ukuran_sepatu = int((100/2)-12)
+print ("Ukuran sepatuku adalah,", Ukuran_sepatu)
+print ("Sepatu favoritku berwarna putih.")
+print ("Aku menyukai sepatu putih karena itu adalah warna kesukaanku.")
+#Menampilkan postur tubuh
+Tinggi_badan = float((200 / 2) + (25 * 2) + 3 )
+Berat_badan = float (40 + 3 )
+print ("Tinggi badanku adalah",Tinggi_badan,".")
+print ("Berat badanku adalah",Berat_badan,".")
